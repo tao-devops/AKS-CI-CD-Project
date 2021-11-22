0333fc07fcf007885acbf255ad19383636171e6a
SDA <57034537+tao-devops@users.noreply.github.com>
2021-11-22 03:00:56 +0100

    Update commands.sh

--- terraform/commands.sh
+++ terraform/commands.sh
@@ -65,7 +65,7 @@ find . -name "frontend-deployment.yaml"
  exit 2
  else
 
-#kubectl apply   -f $(find . -name "frontend-deployment.yaml")
+kubectl apply   -f $(find . -name "frontend-deployment.yaml")
 fi
 #curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 #chmod 700 get_helm.sh
